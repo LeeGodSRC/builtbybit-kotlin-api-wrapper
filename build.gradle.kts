fddf028af5d86e3b5b3485c5a3e3fc29eff6f717
LeeGodSRC <lee20040919@gmail.com>
2022-11-30 19:33:48 +0800

    Added maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    `maven-publish`
 }
 
 group = "dev.imanity.bbbapi"
@@ -16,6 +17,18 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(libs.bundles.ktor.client)
     implementation(libs.coroutines)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.imanity.bbbapi"
+            artifactId = "bbbapi"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
