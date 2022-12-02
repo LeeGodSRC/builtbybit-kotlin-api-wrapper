458309149ee7df989df244dcb08f32269b2d8199
LeeGodSRC <lee20040919@gmail.com>
2022-12-03 01:44:48 +0800

    fix: How did I import the wrong Member class

--- src/main/kotlin/dev/imanity/bbbapi/request/impl/member/RetrieveMemberByDiscordIDRequest.kt
+++ src/main/kotlin/dev/imanity/bbbapi/request/impl/member/RetrieveMemberByDiscordIDRequest.kt
@@ -1,8 +1,8 @@
 package dev.imanity.bbbapi.request.impl.member
 
+import dev.imanity.bbbapi.model.Member
 import dev.imanity.bbbapi.request.Method
 import dev.imanity.bbbapi.request.Request
-import java.lang.reflect.Member
 
 class RetrieveMemberByDiscordIDRequest(
     discordId: Long
